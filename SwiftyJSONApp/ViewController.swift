921ad9577b7743e3194b79b7404b97ddeba7e388
Francesca <fharo@belatrixsf.com>
2019-01-14 08:50:21 -0500

    Initial Commit

---
+++ SwiftyJSONApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyJSONApp
+//
+//  Created by Francesca Valeria Haro Dávila on 1/14/19.
+//  Copyright © 2019 Belatrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
